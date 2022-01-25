637b2e032b486b4fa4d330380c651b0a8790ef32
sz_tamas <szamel.tamas9@gmail.com>
2022-01-25 11:34:48 +0100

    Auto stash before merge of "main" and "origin/main"

--- Model/dominant-vectors.py
+++ Model/dominant-vectors.py
@@ -28,56 +28,6 @@ class Matrix:
         data.columns.name = "Length"
         return str(data)
 
-<<<<<<< Updated upstream
-
-# peti
 # test_arr = [[10, 0, 0], [10, 5, 0], [0, 10, 20]]
 # test = Matrix([40,60,80], test_arr)
 # print(test)
-=======
-        
-test_arr = [[10, 0, 0], [10, 5, 0], [0, 10, 20]]
-test = Matrix([40,60,80], test_arr)
-print(test)
-
-class Hasse:
-    def __init__(self, vector):
-        self.vector = vector
-    
-    @property
-    def vector(self):
-        return self._vector
-    @vector.setter
-    def vector(self, value):
-        self._vector = value
-
-    def powerset(self):
-        s = list(self.vector)
-        return chain.from_iterable(combinations(s, r) for r in range(1,len(s)+1)) # no empty set
-
-    def is_subset(self, set, element):
-        for i in set:
-            if i.sort() == element.sort():
-                return True
-        return False
-
-    def dominant_vectors(self):
-        res = list()
-        # add starting vectors TODO: separate function
-        for i in self.vector:
-            res.append([i])
-        print(res)
-
-        # add combinations TODO: separate function
-        for i in range(3):
-            for j in self.vector:
-                temp = list()
-                temp.append(self.vector[i]), temp.append(j)
-                # check for subset
-                if not self.is_subset(res, temp):
-                    res.append(temp)
-        print(res)
-
-h = Hasse([40,60,80])
-h.dominant_vectors()
->>>>>>> Stashed changes
