48eaccd0b76e54cd18563a82eaa0ab8a6548016e
BMargareta <98470980+BMargareta@users.noreply.github.com>
2022-02-26 21:30:38 +0100

    numpy arrays instead of dictionaries

--- Adatbázis/adatbazis.py
+++ Adatbázis/adatbazis.py
@@ -1,65 +1,86 @@
+import numpy as np
 from pathlib import Path
-def read(): # beolvassa az adatbázisból az összes sort és a kijelölt (fontosnak tartott) oszlopokat
-    scanned = []                          
+
+def read(): # beolvassa az adatbázisból az összes sort és a kijelölt (fontosnak tartott) oszlopokat                         
     p = Path(__file__).with_name('gyartastervezes_csv3.csv')
     with p.open('r',encoding="utf-8") as file:
         content = file.readlines()
-        for row in content:
-            row = row.strip()
-            column = row.split(";")
-            data = {"assembly":column[1], "length":column[5], "width": column[7], "project_id": column[13]}
-            scanned.append(data)  
+        scanned = np.zeros((len(content), 5)).astype(str)
+        for i in range (0, len(content)):
+            content[i] = content[i].strip().split(";")
+            data = np.array([content[i][1], content[i][5], content[i][7], content[i][10], content[i][13]])
+            date = data[3].split(".")
+            if len(date) == 3:
+                j = 0
+                for k in range (0, 3):
+                    if date[k].isdigit() and int(date[k]) > 0:  j += 1  
+                if j == 3: 
+                    data[3] = np.datetime64(data[3].replace(".","-"))
+            scanned[i]=data
     return scanned         
-
+   
 def select (input, a, b): # kiválasztja a beolvasott adatbázisból a kelyheket assembly alapján
     output = []
     for i in range(1,len(input)):
-        data = input[i]
-        assembly = data["assembly"]
-        num = True
-        for char in assembly:
-            if char not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-                num = False
-        if num:
+        assembly = input[i][0]
+        if assembly.isdigit() == True:
             assembly = int(assembly)
             if assembly >= a and assembly <= b:
                 output.append(input[i])
-    return output                 
-
-def elemszam (input): # kigyűjti az előforduló projekteket és a gyakoriságukat
-    gyakorisag = dict()
+    output = np.array(output)
+    return output    
+                     
+def project (input): # kigyűjti az előforduló projekteket és a gyakoriságukat
+    projects = dict()
     for i in range(1,len(input)): 
-        adat = input[i]
-        id = adat["project_id"]
-        if id not in gyakorisag: gyakorisag[id]=1
-        else: gyakorisag[id]+=1
-    return gyakorisag  
+        id = input[i][4]
+        if id not in projects: projects[id]=1
+        else: projects[id]+=1
+    projects = np.array(list(projects.items()))
+    return projects
 
 def sizes(input, project_id): # adott projektben adott hosszúságú és szélességű kelyhek előfordulása
     size = {}
     for i in range(1,len(input)):
-        data = input[i]
-        if data["project_id"] == project_id:
-            s = str(data["length"]) + "x" + str(data["width"])
+        if input[i][4] == project_id:
+            s = str(input[i][1]) + ";" + str(input[i][2])
             if s not in size: size[s] = 1 
             else: size[s] += 1
+    size2 = list(size.keys())
+    for j in range (0, len(size2)):
+        size2[j] = size2[j].split(";")
+    size2 = np.array(size2).reshape(-1,2)
+    frequency = np.array(list(size.values())).reshape(-1,1)
+    output = np.hstack((size2, frequency)).astype(float)    
     p = Path(__file__).with_name('meret.txt')
-    with p.open('w') as file:  # Az eredményt kiírjuk a kimeneti fájlba
-        file.write(str(size))     
-    return size
+    with p.open('w') as file:
+        file.write(str(output))  # length, width, gyakorisag   
+    return output
 
-def convert_into_matrix(_dict):
-    vectors = set()
-    for k,v in _dict.items():
-        if k.split('x')[0] not in vectors:
-            vectors.add(k.split('x')[0])
-    print(list(vectors))
+def time(input, project_id): # gyártáshoz szükséges idő
+    date = []
+    for i in range(1,len(input)):
+        if input[i][4] == project_id:
+            if input[i][3] not in date:
+                date.append(input[i][3])          
+    min = np.datetime64(date[0])
+    max = np.datetime64(date[0])
+    for i in range(1, len(date)):
+        x = np.datetime64(date[i])
+        y = np.datetime64(date[i-1])
+        if x > y : max = x
+        elif x < y : min = x
+    delta = max - min
+    return delta
 
 beolvasott = read()   
+#print(beolvasott)
 a = 1000; b = 1100
 kehely = select(beolvasott, a, b)
-gyakorisag = elemszam(kehely)
-meret = sizes(kehely, 'P19327')
-
+#print(kehely)
+gyakorisag = project(beolvasott)
+meret = sizes(kehely, 'P21138')
+t = time(kehely, 'P21138')
+print(t)
 print(meret)
-convert_into_matrix(meret)
+
